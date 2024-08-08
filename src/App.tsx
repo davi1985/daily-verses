2edad9ab808ba5b830a939e0f2f41dea0c043fb7
Davi Silva <davisilvaphoto@gmail.com>
2024-08-08 20:41:47 -0300

    initial files

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import { Button } from './components/ui/button'
+
+export const App = () => (
+  <div className="flex justify-center items-center h-screen">
+    <Button>Ler meus versos preferidos!</Button>
+  </div>
+)
